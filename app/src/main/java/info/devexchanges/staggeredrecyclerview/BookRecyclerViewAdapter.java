ebe3f63c6d377e68e35d7af29409634e10a940b2
DevExchanges <wandered.vip.007@gmail.com>
2016-07-20 11:21:25 +0700

    update project

--- app/src/main/java/info/devexchanges/staggeredrecyclerview/BookRecyclerViewAdapter.java
+++ app/src/main/java/info/devexchanges/staggeredrecyclerview/BookRecyclerViewAdapter.java
@@ -6,48 +6,56 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
 public class BookRecyclerViewAdapter extends RecyclerView.Adapter<BookRecyclerViewAdapter.BookViewHolder> {
 
-    private List<Book> itemList;
+    private List<Book> bookList;
     private Context context;
 
     public BookRecyclerViewAdapter(Context context, List<Book> itemList) {
-        this.itemList = itemList;
+        this.bookList = itemList;
         this.context = context;
     }
 
     @Override
     public BookViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_grid, null);
-        BookViewHolder rcv = new BookViewHolder(layoutView);
-        return rcv;
+        View layoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_grid, null, false);
+        return new BookViewHolder(layoutView);
     }
 
     @Override
-    public void onBindViewHolder(BookViewHolder holder, int position) {
-        holder.bookName.setText(itemList.get(position).getName());
-        holder.author.setText(itemList.get(position).getAuthor());
+    public void onBindViewHolder(BookViewHolder holder, final int position) {
+        holder.bookName.setText(bookList.get(position).getName());
+        holder.author.setText(bookList.get(position).getAuthor());
+
+        holder.container.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "You clicked at position: " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return this.itemList.size();
+        return this.bookList.size();
     }
 
     public class BookViewHolder extends RecyclerView.ViewHolder {
 
-        public TextView bookName;
-        public TextView author;
+        private TextView bookName;
+        private TextView author;
+        private View container;
 
         public BookViewHolder(View itemView) {
             super(itemView);
 
             bookName = (TextView) itemView.findViewById(R.id.book_name);
             author = (TextView) itemView.findViewById(R.id.author);
+            container = itemView.findViewById(R.id.card_view);
         }
     }
 }
--- app/src/main/java/info/devexchanges/staggeredrecyclerview/MainActivity.java
+++ app/src/main/java/info/devexchanges/staggeredrecyclerview/MainActivity.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private StaggeredGridLayoutManager gaggeredGridLayoutManager;
+    private StaggeredGridLayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,25 +21,31 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
 
-        gaggeredGridLayoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
-        recyclerView.setLayoutManager(gaggeredGridLayoutManager);
+        layoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
 
-        List<Book> gaggeredList = getListItemData();
+        List<Book> bookList = getListItemData();
 
-        BookRecyclerViewAdapter rcAdapter = new BookRecyclerViewAdapter(MainActivity.this, gaggeredList);
-        recyclerView.setAdapter(rcAdapter);
+        BookRecyclerViewAdapter adapter = new BookRecyclerViewAdapter(MainActivity.this, bookList);
+        recyclerView.setAdapter(adapter);
     }
 
     private List<Book> getListItemData(){
 
         List<Book> listViewItems = new ArrayList<>();
         listViewItems.add(new Book("1984", "George Orwell"));
+        listViewItems.add(new Book("Lão Hạc", "Nam Cao"));
+        listViewItems.add(new Book("Kafka on the shore", "Haruki Murakami"));
         listViewItems.add(new Book("Pride and Prejudice", "Jane Austen"));
+        listViewItems.add(new Book("Mảnh trăng cuối rừng", "Nguyễn Minh Châu"));
         listViewItems.add(new Book("One Hundred Years of Solitude", "Gabriel Garcia Marquez"));
         listViewItems.add(new Book("The Book Thief", "Markus Zusak"));
         listViewItems.add(new Book("The Hunger Games", "Suzanne Collins"));
+        listViewItems.add(new Book("Số đỏ", "Vũ Trọng Phụng"));
         listViewItems.add(new Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams"));
         listViewItems.add(new Book("The Theory Of Everything", "Dr Stephen Hawking"));
+        listViewItems.add(new Book("Đàn ghi-ta của Lorca", "Thanh Thảo"));
+        listViewItems.add(new Book("The Trial", "Franz Kafka"));
 
         return listViewItems;
     }
